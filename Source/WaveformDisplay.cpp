d0b3cc28b9e03b98a03915a42b6e8037b3b2bd75
Y. Nagy <youssefnaggi@gmail.com>
2026-05-02 20:27:44 +0300

    Update WaveformDisplay.cpp

--- Source/WaveformDisplay.cpp
+++ Source/WaveformDisplay.cpp
@@ -18,16 +18,16 @@ WaveformDisplay::~WaveformDisplay()
 
 void WaveformDisplay::paint(juce::Graphics& g)
 {
-    // خلفية
+
     g.fillAll(juce::Colours::black);
 
-    // حدود
+
     g.setColour(juce::Colours::grey);
     g.drawRect(getLocalBounds(), 1);
 
     if (fileLoaded && thumbnail.getTotalLength() > 0)
     {
-        // رسم الموجة الصوتية
+
         g.setColour(juce::Colours::cyan);
         thumbnail.drawChannels(g,
             getLocalBounds(),
@@ -35,18 +35,17 @@ void WaveformDisplay::paint(juce::Graphics& g)
             thumbnail.getTotalLength(),
             1.0f);
 
-        // رسم مؤشر الموضع الحالي
         g.setColour(juce::Colours::red);
         int xPosition = position * getWidth();
         g.drawLine(xPosition, 0, xPosition, getHeight(), 2.0f);
 
-        // رسم دائرة صغيرة عند المؤشر
+         
         g.setColour(juce::Colours::yellow);
         g.fillEllipse(xPosition - 3, getHeight() - 6, 6, 6);
     }
     else
     {
-        // رسالة إذا مفيش ملف محمل
+        
         g.setColour(juce::Colours::white);
         g.setFont(16.0f);
         g.drawText("Load audio file to display waveform",
@@ -58,7 +57,7 @@ void WaveformDisplay::paint(juce::Graphics& g)
 
 void WaveformDisplay::resized()
 {
-    // ممكن تضيفي تعديلات هنا إذا محتاجة
+    
 }
 
 void WaveformDisplay::loadFile(const juce::File& audioFile)
@@ -84,7 +83,7 @@ void WaveformDisplay::changeListenerCallback(juce::ChangeBroadcaster* source)
 {
     if (source == &thumbnail)
     {
-        // الرخصة لما الـ thumbnail يخلص تحميل البيانات
+         
         repaint();
     }
 }
